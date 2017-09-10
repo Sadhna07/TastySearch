df4668d5a64b23a10b9faa278d1b5c843b211ab8
Sadhna07 <sadhnanarayan.sadhna@gmail.com>
2017-09-10 22:36:08 +0530

    search Engine completed

--- app.py
+++ app.py
@@ -1,11 +1,88 @@
 from __future__ import division
-from flask import Flask, request, render_template
-import random
+from flask import Flask, request, render_template, jsonify, flash, session
+import random, os
 import numpy as np
+from sample import *
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug = True)
+def search(token):
+	try:
+		# print token
+		token = [x.lower().strip() for x in token.split(',')]
+		if len(token) != 0:
+			index = 0
+			searchResult = []
+			documents = []
+			length = len(token)
+			keywordSearch = [keyword[elem] for elem in token if elem in keyword.keys()] 
+			keywordSearch = set().union(*keywordSearch)
+			# print keywordSearch, len(keywordSearch)
 
-        
+			if len(keywordSearch) == 0:
+				return ['Empty Search']
+
+			result = [0] * (max(keywordSearch) + 1)
+			# print keywordSearch
+			for x in keywordSearch:
+				count = 0
+				for y in token:
+					if y in reviewVal[x]:
+						count += 1
+					else:
+						pass
+				result[x] = count / length
+			final_result = list((np.argsort(result)[::-1])[:20])
+
+			i = 0
+			for x in final_result:
+				if result[x] != 0:
+					searchResult.append(str(reviewStringsVal[x]) + 'Score: ' + str(result[x]) )
+					i = i + 1
+				else:
+					pass
+				
+			return searchResult
+			pass
+		else:
+			return ['Empty Search']
+	except Exception, e:
+		return str(e)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	try:
+		if request.method == 'POST':
+			searchKeyword = request.form['search']
+			searchResult = search(searchKeyword)
+			return render_template('result.html', data=searchKeyword, var=searchResult)
+		else:
+			return render_template('index.html')
+	except Exception, e:
+		flash(e)
+
+
+@app.route('/result')
+def result():
+	return render_template('result.html')
+
+
+@app.route('/api', methods=['GET'])
+def api():
+	try:
+		searchKeyword = request.args.get('searchKeyword')
+		searchResult = search(searchKeyword)
+		searchEngine = {'searchKeyword': searchKeyword, 'searchResult': searchResult}
+		return (jsonify({'searchEngine': searchEngine}), 200)
+	except Exception, e:
+		return (str(e) + 'ERROR' + str(searchResult), 400)
+		flash(e)
+
+
+
+if __name__ == '__main__':
+	app.run(debug = True, secret_key = 'secret11232234535465676b')
+
+			
---
+++ sample.py
@@ -0,0 +1,60 @@
+import random
+
+
+# Sampling
+indexVal = 0
+reviewVal = []
+reviewStringsVal = []
+keyword = {}
+data = ''
+dataString = ''
+index = 0
+rand = random.randint(1, 11)
+position = (rand - 1) * 9
+position = 0
+docCount = 0
+with open('foods.txt', 'r') as infile:
+    for (indexVal, myString) in enumerate(infile):
+        myString = unicode(myString, errors='ignore')
+        if indexVal >= position:
+            # print indexVal, myString
+            if myString and myString.strip():
+                dataString = dataString + '\n' + myString
+                array = myString.split(':')
+                if array[0] == 'review/text' or array[0] == 'review/summary':
+                    data = data + array[1]
+            else:
+                docCount += 1
+                x = [x.lower().strip() for x in data.split()]
+                a = set()
+                for elem in x:
+                    a.add(elem)
+                    if elem not in keyword.keys():
+                        temp = set()
+                        temp.add(index )
+                        keyword[elem] = temp
+                    else:
+                        keyword[elem].add(index)
+                index += 1
+                reviewVal.append(a)
+                reviewStringsVal.append(dataString)
+
+                data = ''
+                dataString = ''
+            if docCount == 100000:
+                break
+        else:
+            continue
+
+keywordFile = open('keyword.txt', 'w+')
+search_url = 'http://127.0.0.1:5000/api?search_token='
+i = 0
+while i < 100000:
+    N = random.randint(1, 11)
+    x = random.sample(keyword.keys(), N)
+    line = ','.join(x)
+    line = search_url + line
+    keywordFile.write(line)
+    keywordFile.write('\n')
+    i = i + 1
+keywordFile.close()
