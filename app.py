be69d93ce0be4f9c79bbc0aff5a6746cf2a8fb6d
Sadhna07 <sadhnanarayan.sadhna@gmail.com>
2017-09-10 18:05:04 +0530

    main application file created

---
+++ app.py
@@ -0,0 +1,11 @@
+from __future__ import division
+from flask import Flask, request, render_template
+import random
+import numpy as np
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+        
